296b0f926b40d45f9e132307057addb78c7598d5
zerostarm <zerostarm@users.noreply.github.com>
2017-10-16 12:58:04 -0400

    I made it work with the same number of characters output

--- src/Gen.java
+++ src/Gen.java
@@ -5,51 +5,55 @@
  * @author Zeros. Created Oct 11, 2017.
  */
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Gen {
-
+	
+	private final int size = 127;
+	
 	Gen() {
-		char[] a = new char[96];
+		char[] a = new char[size];
 		int d = 0;
 
-		for (char c = ' '; c <= '`'; c++, d++) {
+		for (char c = ' '; c <= '~'; c++, d++) {//makes a character array with all of the characters in it
 			a[d] = c;
 
 		}
-		for (int v = 0; v < 95; v++) {
+		int v = 0;
+		for (; v < size; v++) { //prints that character array
 			System.out.print(a[v]);
 
 		}
-		
+		//System.out.println(v); prints the number of array positions it printed 
 		
 		String str = new String();
-		int[] ints = new int[95];
+		int[] array = new int[size];
 
-		for (int v = 0; v < 95; v++) {
-			ints[v] = v;
+		for (v = 0; v < size; v++) { //creates an integer array with all values from 0 to 127
+			array[v] = v;
 
 		}
 		
-		shuffleArray(ints);
+		array = Gen.RandomizeArray(array);
 
-		for (d = 0; d < 95; d++) {
-			str = str + a[ints[d]];
-
+		for (d = 0; d < 95; d++) {// turns the shuffled letters in to an array. 
+			char hi = a[array[d]];
+			str = str + "" +  hi;
+			str = str.trim();
 		}
 		System.out.print(str);
 
 	}
 
-	static void shuffleArray(int[] ar) {
-		// If running on Java 6 or older, use `new Random()` on RHS here
-		Random rnd = ThreadLocalRandom.current();
-		for (int i = ar.length - 1; i > 0; i--) {
-			int index = rnd.nextInt(i + 1);
-			// Simple swap
-			int a = ar[index];
-			ar[index] = ar[i];
-			ar[i] = a;
+	public static int[] RandomizeArray(int[] array){
+		Random rgen = new Random();  // Random number generator			
+ 
+		for (int i=0; i<array.length; i++) {
+		    int randomPosition = rgen.nextInt(array.length);
+		    int temp = array[i];
+		    array[i] = array[randomPosition];
+		    array[randomPosition] = temp;
 		}
+ 
+		return array;
 	}
 }
--- src/Input.java
+++ src/Input.java
@@ -5,12 +5,85 @@
  * @author Zeros.
  *         Created Oct 11, 2017.
  */
+import java.util.*;
+import java.io.*;
+
 public class Input {
-	
-	public static void main (String[] args) { 
-		Gen a = new Gen(); 
-		
-		
+
+	public static void main(String[] args) throws java.io.FileNotFoundException {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Input your password to be encrypted: ");
+		String alpha = scan.next();
+
+		String beta = Input.Find(alpha);
+		System.out.println(beta);
 	}
 
+	private static final int size = 127;
+
+	public static String Find(String alpha) throws java.io.FileNotFoundException {// needs to take the character at index x of alpha and replace it with the
+												// character at index x of ceta.
+
+		String ceta = Input.Gen();
+		String newn = "";
+		int a = 0;
+		while(a<alpha.length()) {
+		//for (int a = 0; a < alpha.length(); a++) {
+			newn = newn + ceta.charAt(a);
+			a+=1;
+		}
+		System.out.println(newn);
+		try(  PrintWriter out = new PrintWriter( "filename.txt" )  ){
+		    out.println( ceta );
+		}
+		return newn;
+	}
+
+	public static String Gen() {
+		char[] a = new char[size];
+		int d = 0;
+
+		for (char c = ' '; c <= '~'; c++, d++) {// makes a character array with all of the characters in it
+			a[d] = c;
+
+		}
+		int v = 0;
+		// (; v < size; v++) { // prints that character array
+		// System.out.print(a[v]);
+
+		// }
+		// System.out.println(v); prints the number of array positions it printed
+
+		String str = new String();
+		int[] array = new int[size];
+
+		for (v = 0; v < size; v++) { // creates an integer array with all values from 0 to 127
+			array[v] = v;
+
+		}
+
+		array = Input.RandomizeArray(array);
+
+		for (d = 0; d < size; d++) {// turns the shuffled letters in to an array.
+			char hi = a[array[d]];
+			str = "" + str + "" + hi;
+			str = str.trim();
+		}
+		return str;
+
+	}
+
+	public static int[] RandomizeArray(int[] array) {
+		Random rgen = new Random(); // Random number generator
+
+		for (int i = 0; i < array.length; i++) {
+			int randomPosition = rgen.nextInt(array.length);
+			int temp = array[i];
+			array[i] = array[randomPosition];
+			array[randomPosition] = temp;
+		}
+
+		return array;
+	}
 }
